dc41bc91708ed3057377991514b4462c03672b65
franciskumar <31071848+franciskumar@users.noreply.github.com>
2017-08-17 00:48:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Testing OPSWORKS</title>
+    </head>
+
+    <body >
+        <div align="center">
+            <h1>Testing Opswork Demo</h1>
+            <h2>YIPEE! You have successfully installed on Opsworks!</h2>
+            <div align="center"><img src="https://s3-eu-west-1.amazonaws.com/fmlabsdnd/lle.jpg"></div>
+            <p>My Application is running on host &nbsp;<?php echo gethostname(); ?>&nbsp; in a new stack and layer in OpsWorks.</p>
+            <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
